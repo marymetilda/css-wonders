0906daacfbac82cf4e60162805c1cb07d123f059
mary-metilda <mary.metilda@vonnue.com>
2022-11-20 01:08:14 +0530

    feat: switch html

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="switch">
+     <div>
+      <i />
+      <span>Blootooth</span>
+     </div>
+    <div>
+      <input type="checkbox" />
+      <label htmlFor="bluetooth"></label>
+    </div>
+    </div>
+  );
+}
+
+export default App;
